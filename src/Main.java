6b7a21120ae66a5f93682ffe54ee99bed5df8649
Calyen93 <genco.claudio3@gmail.com>
2024-02-14 16:32:10 +0100

    Create multiplication feature

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(multiplication(2, 10));
     }
 
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
 }
