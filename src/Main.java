4fad8b2393f85d5274859927f439d386fc7e13e8
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-02-14 15:50:26 +0100

    Created the sum function

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(sum(2, 4));
+    }
 
+    public static int sum(int a, int b) {
+        return a + b;
     }
 
 }
