fdfc19b33b46c40a9a09f8259c7c77572dfa3080
Calyen93 <genco.claudio3@gmail.com>
2024-02-15 15:54:43 +0100

    Sub Feature

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(sub(10, 3));
     }
 
+    public static int sub(int a, int b) {
+        return a - b;
+
+    }
 }
