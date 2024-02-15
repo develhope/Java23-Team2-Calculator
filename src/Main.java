d0add2b650d8b7b186a0d2dc4e17eb0725f4253d
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-02-15 15:43:31 +0100

    new feature to recognise type of calc implemented

--- src/Main.java
+++ src/Main.java
@@ -15,4 +15,32 @@ public class Main {
     public static int multiplication(int a, int b) {
         return a * b;
     }
+    public static String operatorKind(String operation) {
+        String output = "";
+        int i = 0;
+        if (operation.indexOf("+") > 0) {
+            output = "sum";
+            i++;
+        }
+        if (operation.indexOf("-") > 0) {
+            output = "sub";
+            i++;
+        }
+        if (operation.indexOf("*") > 0) {
+            output = "multiplication";
+            i++;
+        }
+        if (operation.indexOf("/") > 0) {
+            output = "division";
+            i++;
+        }
+        if (operation.indexOf("^") > 0) {
+            output = "exponents";
+            i++;
+        }
+        if (i == 1) {
+            return output;
+        } else return "Error";
+    }
+
 }
