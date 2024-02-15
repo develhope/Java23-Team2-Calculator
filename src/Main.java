8526320c685bac5a8ad57be3bd02b06f23ee03e3
Sergio <sergio.hanganu@gmail.com>
2024-02-15 15:17:04 +0100

    Division method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(division(12, 3));
 
     }
 
+    public static int division(int a, int b) {
+        return a / b;
+
+    }
 }
+
