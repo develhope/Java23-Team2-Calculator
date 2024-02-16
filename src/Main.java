1219b96b05efab7f7cc128cb7bcb83ff2a1458a7
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-02-16 18:03:15 +0100

    Implemented the function thta given a string calls the right method and return the result

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,19 @@ public class Main {
         System.out.println(sub(10, 3));
         System.out.println(sum(2, 4));
         System.out.println(multiplication(2, 10));
-
+        System.out.println(calculator("1*a"));
     }
-
 
     public static int sub(int a, int b) {
         return a - b;
-
     }
 
     public static int division(int a, int b) {
         return a / b;
+    }
 
     public static int sum(int a, int b) {
         return a + b;
-
     }
 
     public static int sum(int[] values) {
@@ -33,7 +31,6 @@ public class Main {
     public static int multiplication(int a, int b) {
         return a * b;
     }
-
 
     public static String operatorKind(String operation) {
         String output = "";
@@ -61,6 +58,44 @@ public class Main {
         if (i == 1) {
             return output;
         } else return "Error";
-
     }
+
+    public static Integer calculator(String operation) {
+        // Rimuove gli spazi dalla stringa
+        operation = operation.replace(" ", "");
+        // Crea un array contenente i vari fattori dell'operazione
+        String[] elementsString = operation.split("[+-/^*]");
+        // Controlla che element[] sia int e se si lo immagizina in un array di interi.
+        boolean elementsAreNotInt = false;
+        int[] elements = new int[elementsString.length];
+        for (int i = 0; i < elementsString.length; i++) {
+            try {
+                Integer.parseInt(elementsString[i]);
+                elements[i] = Integer.valueOf(elementsString[i]);
+            } catch (Exception e) {
+                elementsAreNotInt = true;
+            }
+        }
+
+        if (elementsAreNotInt) {
+            System.out.print("Error, looks like something went wrong. Remember that u can just operate one operation at the time. Return: ");
+            return null;
+        } else {
+            switch (operatorKind(operation)) {
+                case "sum":
+                    return sum(elements);
+                case "sub":
+                    return sub(elements[0], elements[1]);
+                case "multiplication":
+                    return multiplication(elements[0], elements[1]);
+                case "division":
+                    return division(elements[0], elements[1]);
+                case "error":
+                    System.out.println("Error, looks like something went wrong. Remember that u can just operate one operation at the time on Int.");
+                    break;
+            }
+        }
+        return null;
+    }
+
 }
