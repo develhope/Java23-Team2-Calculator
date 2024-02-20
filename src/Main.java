a8a105fd33080881563b5fd03d177c7e6a1664e0
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-02-20 21:07:33 +0100

    Calculus funzionante, da stressare alla ricerca di bug

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        Calculus calculus2 = new Calculus("1-3*2-2");
+        Calculus calculus2 = new Calculus("1+3*2/3^2");
         System.out.println(calculus2);
     }
 }
