479af6e2be363b597032f38b012211988ba17284
Worro <alessiodelledonne29@gmail.com>
2024-02-23 14:08:57 +0100

    Implemented some exemples of subtraction in Main.java and changed integers into double

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import Operations.Subtraction;
+
 public class Main {
     public static void main(String[] args) {
+        //Array di prova
+        double[] numbers = {50, 20, 3};
+        double[] otherNumbers = {5, 7, 2};
+        double[] negatives = {-5, -3, -10};
+        //Stampa sottrazioni a due elementi.
+        System.out.println("Stampa sottrazioni a due elementi.");
+        System.out.println(Subtraction.sub(2, 3));
+        System.out.println(Subtraction.sub(5, 3));
+        System.out.println(Subtraction.sub(-5, 3));
+        System.out.println();
+        //Stampa sottrazioni tra elementi di arrays
+        System.out.println("Stampa sottrazioni tra elementi di arrays");
+        System.out.println(Subtraction.sub(numbers));
+        System.out.println(Subtraction.sub(otherNumbers));
+        System.out.println(Subtraction.sub(negatives));
+        System.out.println();
+
+
         Calculus calculus2 = new Calculus("1+3*2/3^2");
         System.out.println(calculus2);
     }
--- src/Operations/Subtraction.java
+++ src/Operations/Subtraction.java
@@ -3,13 +3,13 @@ package Operations;
 public class Subtraction {
 
     //Metodo sottrazione a più elementi
-    public static int sub(int a, int b) {
+    public static double sub(double a, double b) {
         return a - b;
     }
 
     //Metodo sottrazione degli elementi di un array
-    public static int sub(int[] values) {
-        int result = values[0];
+    public static double sub(double[] values) {
+        double result = values[0];
         for (int i = 1; i < values.length; i++) {
             result -= values[i];
         }
