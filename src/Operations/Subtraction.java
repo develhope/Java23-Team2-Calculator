56ddc9ccfbfa48a3f7b9a52303246eca32af8895
Worro <alessiodelledonne29@gmail.com>
2024-02-23 03:38:07 +0100

    Created the Opeartions package where to store all the oparations classes

--- src/Operations/Subtraction.java
+++ src/Operations/Subtraction.java
@@ -1,10 +1,14 @@
+package Operations;
 
 public class Subtraction {
+
+    //Metodo sottrazione a più elementi
     public static int sub(int a, int b) {
         return a - b;
     }
 
-    static int sub(int[] values) {
+    //Metodo sottrazione degli elementi di un array
+    public static int sub(int[] values) {
         int result = values[0];
         for (int i = 1; i < values.length; i++) {
             result -= values[i];
