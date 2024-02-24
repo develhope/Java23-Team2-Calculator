d4d0dcfbad227cfd3f26848799a49e42cceaf704
Worro <alessiodelledonne29@gmail.com>
2024-02-24 19:32:40 +0100

    Now Subtraction extends calc and has many tests.

--- src/Operations/Subtraction.java
+++ src/Operations/Subtraction.java
@@ -2,7 +2,21 @@ package Operations;
 
 public class Subtraction extends Calc {
     public static void main(String[] args) {
-
+        int[] array = {10, 5, 3};
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {5, 2, -6};
+        Calc test = new Subtraction(1, 2);
+        Calc test1 = new Subtraction(3, 2);
+        Calc test2 = new Subtraction(5, -7);
+        Calc testArr = new Subtraction(array);
+        Calc testArr1 = new Subtraction(array1);
+        Calc testArr2 = new Subtraction(array2);
+        System.out.println(test);
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(testArr);
+        System.out.println(testArr1);
+        System.out.println(testArr2);
     }
 
     public Subtraction(int a, int b) {
